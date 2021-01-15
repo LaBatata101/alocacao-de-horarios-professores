35f780c2cecd02b218fc0480ad3186999c9b420b
LaBatata101 <labatata101@linuxmail.org>
2021-01-15 19:10:00 -0300

    add functions to create the period restriction logic formula
    dont_allow_on_the_same_period: return a formula that doesn't allow
    courses to be at the same period
    
    period_restriction: creates the final formula for one course
    
    period_restriction_for_all_semesters_formula: creates the formula using
    period_restriction for all courses

--- alocacao.py
+++ alocacao.py
@@ -1,3 +1,4 @@
+from itertools import combinations
 from formula import And, Atom, Not, Or
 from typing import List, Dict, Union, Any
 
@@ -86,3 +87,67 @@ def or_all(logic_formulas) -> Or:
     for i in range(1, len(logic_formulas)):
         ors = Or(ors, logic_formulas[i])
     return ors
+
+
+
+def dont_allow_on_the_same_period(atomics: List[Atom]) -> Union[And, List[Atom]]:
+    if len(atomics) <= 1:
+        return atomics
+
+    temp = []
+    for c in combinations(atomics, 2):
+        temp.append(Not(and_all(c)))
+    return and_all(temp)
+
+
+def period_restriction(courses_atoms: List[Atom]) -> And:
+    """
+    Same as period_restriction_for_all_semesters_formula but only returns one formula from a list of Atom's.
+    """
+    first_period, second_period = [], []
+    for i in range(len(courses_atoms) // 2):
+        first_period.append(courses_atoms[i * 2])
+        second_period.append(courses_atoms[(i * 2) - 1])
+
+    all_possible_periods = []
+    for i in range(len(first_period)):
+        for j in range(len(first_period)):
+            course_first_period = first_period[i].name.split("_")[0]
+            course_second_period = second_period[j].name.split("_")[0]
+
+            if not course_second_period.startswith(course_first_period):
+                all_possible_periods.append(And(first_period[i], second_period[j]))
+
+    return and_all([dont_allow_on_the_same_period(first_period), dont_allow_on_the_same_period(second_period),
+                    or_all(all_possible_periods)])
+
+
+def period_restriction_for_all_semesters_formula(courses: List[Course]) -> List[Union[And, Or]]:
+    """
+    Take a list of Course objects and create a logic formula that doesn't allow
+    different Course's to be at the same period on the same semester. In the
+    case of this code there will be two periods, the 1º period is from 8 to 10,
+    and the 2º period is from 10 to 12.
+
+    Return a list of logic formulas, each position in the list represents a semester.
+    Example of a possible logic formula of any semester:
+        ~(Circuitos Digitais_1 ^ Fundamentos de Programação_1) ^ ~(Circuitos Digitais_2 ^ Fundamentos de Programação_2) ^
+        (Circuitos Digitais_1 ^ Fundamentos de Programação_2) v (Circuitos Digitais_2 ^ Fundamentos de Programação_1)
+    """
+    atomics_by_semester: Dict[str, List[Atom]] = {}
+    for c in courses:
+        atomics = []
+        for p in (1, 2):  # create a Atom representing the 1º and 2º period of a Course
+            atomics.append(Atom(f"{c.name}_{p}"))
+
+        if c.semester not in atomics_by_semester:
+            atomics_by_semester[c.semester] = []
+        atomics_by_semester[c.semester].extend(atomics)
+
+    final_formula: List[Union[And, Or]] = []
+    for s in atomics_by_semester:
+        if len(atomics_by_semester[s]) == 2:  # XXX: for the sake of gambiarra
+            final_formula.append(Or(atomics_by_semester[s][0], atomics_by_semester[s][1]))
+            continue
+        final_formula.append(period_restriction(atomics_by_semester[s]))
+    return final_formula
