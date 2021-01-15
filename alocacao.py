d097ed6693bd649a6e0d3ce133f0ff8ff0c6c706
LaBatata101 <labatata101@linuxmail.org>
2021-01-15 19:00:38 -0300

    add functions to read and parse use input
    multiline_input: allows the user to input multiline text
    parse_input: parse the input text into Course objects

--- alocacao.py
+++ alocacao.py
@@ -1,3 +1,6 @@
+from typing import List, Dict, Union, Any
+
+
 class Course:
     def __init__(self, name="", semester="", professor=""):
         self.name = name
@@ -11,3 +14,53 @@ class Course:
         return f"Course({self.name}, {self.semester}, {self.professor}"
 
 
+def multiline_input(input_text="") -> List[str]:
+    print(input_text)
+    lines = []
+    while True:
+        try:
+            line = input()
+        except KeyboardInterrupt:
+            pass
+
+        if line:
+            lines.append(line.strip())
+        else:
+            break
+    return lines
+
+
+def parse_input(text: List[str]) -> List[Course]:
+    """
+    Parses the input text and return a list of Course objects
+    """
+    courses = []
+    for i in range(len(text)):
+        if len(text[i]) == 2:
+            course = None
+            semester_str = text[i]
+            count = i + 1
+
+            while count < len(text) and len(text[count]) != 2:
+                course = Course()
+                course.semester = semester_str
+
+                try:
+                    course.name, course.professor = text[count].split(',')
+                    course.professor = course.professor.strip()
+                except ValueError:
+                    print(f"Formato da entrada incorreto!\n\033[31mERRO:\033[m \"{text[count]}\", linha {count + 1}")
+                    print('''\nFormato da entrada:
+                          semestre
+                          nome do curso, nome do professor
+                                    .
+                                    .
+                                    .
+                          nome do curso, nome do professor
+                          ''')
+                    exit(1)
+
+                count += 1
+                courses.append(course)
+
+    return courses
